f502ef4535d97bced9e3e7f48a10719d7066a9ee
blane187gt <167639579+blane187gt@users.noreply.github.com>
2024-09-04 00:19:58 +0700

    Update modules.py

--- rvc_infer/libslor/modules.py
+++ rvc_infer/libslor/modules.py
@@ -5,9 +5,9 @@ from torch.nn import Conv1d
 from torch.nn import functional as F
 from torch.nn.utils import remove_weight_norm, weight_norm
 
-from rvc_infer.infer_libs.infer_pack import commons
-from rvc_infer.infer_libs.infer_pack.commons import get_padding, init_weights
-from rvc_infer.infer_pack.transforms import piecewise_rational_quadratic_transform
+from rvc_infer.libslor import commons
+from rvc_infer.libslor.commons import get_padding, init_weights
+from rvc_infer.libslor.transforms import piecewise_rational_quadratic_transform
 
 LRELU_SLOPE = 0.1
 
