16dcbe500fc162c71f747db299631ec951f9299d
blane187gt <167639579+blane187gt@users.noreply.github.com>
2024-09-04 12:28:46 +0700

    Update __init__.py

--- rvc_infer/__init__.py
+++ rvc_infer/__init__.py
@@ -1,5 +1,5 @@
-from .infer import infer_audio
-from .dlmodels import download_online_model
+from rvc_infer.infer import infer_audio
+from rvc_infer.dlmodels import download_online_model
 
 # some main stuff
 
