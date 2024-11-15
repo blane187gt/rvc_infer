97c54abc5b85d664e1ef444102eba1ba1177b848
Rico Ardiansyah <167639579+blane187gt@users.noreply.github.com>
2024-11-15 11:19:12 +0700

    Update infer.py

--- rvc_infer/infer.py
+++ rvc_infer/infer.py
@@ -1,40 +1,62 @@
 import os
-import shutil
-import gc
+from pydub import AudioSegment
+from pydub.silence import detect_silence, detect_nonsilent
+import shutil, gc, ffmpeg
 import torch
 from multiprocessing import cpu_count
-import os, sys
-import traceback
-import logging
-now_dir = os.getcwd()
-sys.path.append(now_dir)
-logger = logging.getLogger(__name__)
+import sys, traceback, logging
 import numpy as np
 import soundfile as sf
 import torch
 from io import BytesIO
-from lib.infer_libs.audio import load_audio
-from lib.infer_libs.audio import wav2
-from lib.infer_libs.infer_pack.models import (
-    SynthesizerTrnMs256NSFsid,
-    SynthesizerTrnMs256NSFsid_nono,
-    SynthesizerTrnMs768NSFsid,
-    SynthesizerTrnMs768NSFsid_nono,
-)
-from lib.pipeline import Pipeline
+from functools import lru_cache
+from time import time as ttime
+from torch import Tensor
+import faiss
+import librosa
+import numpy as np
+import parselmouth
+import pyworld
+import torch.nn.functional as F
+from scipy import signal
+from tqdm import tqdm
+import random
+import re
+from functools import partial
+import torchcrepe  # Fork Feature. Crepe algo for training and preprocess
+from torchfcpe import spawn_bundled_infer_model
+import torch
 import time
 import glob
 from shutil import move
 from fairseq import checkpoint_utils
-from rvc_infer.split_audio import split_silence_nonsilent, adjust_audio_lengths, combine_silence_nonsilent
-import os
 import zipfile
 import shutil
 import urllib.request
 import gdown
 from pathlib import Path
 import subprocess
-import os
+now_dir = os.getcwd()
+sys.path.append(now_dir)
+logger = logging.getLogger(__name__)
+from rvc_infer.libslor.audio import load_audio
+from rvc_infer.libslor.audio import wav2
+from rvc_infer.libslor.infer_pack.models import (
+    SynthesizerTrnMs256NSFsid,
+    SynthesizerTrnMs256NSFsid_nono,
+    SynthesizerTrnMs768NSFsid,
+    SynthesizerTrnMs768NSFsid_nono,
+)
+from rvc_infer.libslor.rmvpe import RMVPE
+from rvc_infer.libslor.fcpe import FCPE
+from rvc_infer.pipeline import Pipeline
+
+logger = logging.getLogger(__name__)
+
+
+bh, ah = signal.butter(N=5, Wn=48, btype="high", fs=16000)
+
+input_audio_path2wav = {}
 
 
 
@@ -93,6 +115,121 @@ def download_online_model(url, dir_name):
 
     except Exception as e:
         raise Exception(str(e))
+
+
+
+
+SEPERATE_DIR = os.path.join(os.getcwd(), "seperate")
+TEMP_DIR = os.path.join(SEPERATE_DIR, "temp")
+cache = {}
+
+os.makedirs(SEPERATE_DIR, exist_ok=True)
+os.makedirs(TEMP_DIR, exist_ok=True)
+
+def cache_result(func):
+    def wrapper(*args, **kwargs):
+        key = (args, frozenset(kwargs.items()))
+        if key in cache:
+            return cache[key]
+        else:
+            result = func(*args, **kwargs)
+            cache[key] = result
+            return result
+    return wrapper   
+
+def get_non_silent(audio_name, audio, min_silence, silence_thresh, seek_step, keep_silence):
+    """
+    Function to get non-silent parts of the audio.
+    """
+    nonsilent_ranges = detect_nonsilent(audio, min_silence_len=min_silence, silence_thresh=silence_thresh, seek_step=seek_step)
+    nonsilent_files = []
+    for index, range in enumerate(nonsilent_ranges):
+        nonsilent_name = os.path.join(SEPERATE_DIR, f"{audio_name}_min{min_silence}_t{silence_thresh}_ss{seek_step}_ks{keep_silence}", f"nonsilent{index}-{audio_name}.wav")
+        start, end = range[0] - keep_silence, range[1] + keep_silence
+        audio[start:end].export(nonsilent_name, format="wav")
+        nonsilent_files.append(nonsilent_name)
+    return nonsilent_files
+
+def get_silence(audio_name, audio, min_silence, silence_thresh, seek_step, keep_silence):
+    """
+    Function to get silent parts of the audio.
+    """
+    silence_ranges = detect_silence(audio, min_silence_len=min_silence, silence_thresh=silence_thresh, seek_step=seek_step)
+    silence_files = []
+    for index, range in enumerate(silence_ranges):
+        silence_name = os.path.join(SEPERATE_DIR, f"{audio_name}_min{min_silence}_t{silence_thresh}_ss{seek_step}_ks{keep_silence}", f"silence{index}-{audio_name}.wav")
+        start, end = range[0] + keep_silence, range[1] - keep_silence
+        audio[start:end].export(silence_name, format="wav")
+        silence_files.append(silence_name)
+    return silence_files
+
+@cache_result
+def split_silence_nonsilent(input_path, min_silence=500, silence_thresh=-40, seek_step=1, keep_silence=100):
+    """
+    Function to split the audio into silent and non-silent parts.
+    """
+    audio_name = os.path.splitext(os.path.basename(input_path))[0]
+    os.makedirs(os.path.join(SEPERATE_DIR, f"{audio_name}_min{min_silence}_t{silence_thresh}_ss{seek_step}_ks{keep_silence}"), exist_ok=True)
+    audio = AudioSegment.silent(duration=1000) + AudioSegment.from_file(input_path) + AudioSegment.silent(duration=1000)
+    silence_files = get_silence(audio_name, audio, min_silence, silence_thresh, seek_step, keep_silence)
+    nonsilent_files = get_non_silent(audio_name, audio, min_silence, silence_thresh, seek_step, keep_silence)
+    return silence_files, nonsilent_files
+
+def adjust_audio_lengths(original_audios, inferred_audios):
+    """
+    Function to adjust the lengths of the inferred audio files list to match the original audio files length.
+    """
+    adjusted_audios = []
+    for original_audio, inferred_audio in zip(original_audios, inferred_audios):
+        audio_1 = AudioSegment.from_file(original_audio)
+        audio_2 = AudioSegment.from_file(inferred_audio)
+        
+        if len(audio_1) > len(audio_2):
+            audio_2 += AudioSegment.silent(duration=len(audio_1) - len(audio_2))
+        else:
+            audio_2 = audio_2[:len(audio_1)]
+        
+        adjusted_file = os.path.join(TEMP_DIR, f"adjusted-{os.path.basename(inferred_audio)}")
+        audio_2.export(adjusted_file, format="wav")
+        adjusted_audios.append(adjusted_file)
+    
+    return adjusted_audios
+
+def combine_silence_nonsilent(silence_files, nonsilent_files, keep_silence, output):
+    """
+    Function to combine the silent and non-silent parts of the audio.
+    """
+    combined = AudioSegment.empty()
+    for silence, nonsilent in zip(silence_files, nonsilent_files):
+        combined += AudioSegment.from_wav(silence) + AudioSegment.from_wav(nonsilent)
+    combined += AudioSegment.from_wav(silence_files[-1])
+    combined = AudioSegment.silent(duration=keep_silence) + combined[1000:-1000] + AudioSegment.silent(duration=keep_silence)
+    combined.export(output, format="wav")
+    return output
+
+
+
+def load_audio(file, sr):
+    try:
+        # https://github.com/openai/whisper/blob/main/whisper/audio.py#L26
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        file = (
+            file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
+        )  # 防止小白拷路径头尾带了空格和"和回车
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr)
+            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+        )
+    except Exception as e:
+        raise RuntimeError(f"Failed to load audio: {e}")
+
+    return np.frombuffer(out, np.float32).flatten()
+
+
+
+
 
 
 
